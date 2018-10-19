7255cd959719ed8d2c2bf0eee004c4afd6f5e77d
pvonglehn <patrickvonglehn@gmail.com>
2018-10-19 13:25:09 +0000

    cloze deletions working on page. Data still explicit in flask_ap.py

--- flask_app.py
+++ flask_app.py
@@ -2,15 +2,17 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template, redirect, request, url_for
+import random
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-comments = []
-@app.route("/",methods=["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
+        my_pairs = (("Quiero que te (ir, tú) ya","Quiero que te vayas ya"),("Necesito que me (ayuar,tú) con esto","Quiero que me ayudes con esto"))
+        my_pair = my_pairs[random.randrange(len(my_pairs))]
+        front = my_pair[0]
+        back = my_pair[1]
+        return render_template("main_page.html", front=front,back=back)
 
-    comments.append(request.form["contents"])
-    return redirect(url_for('index'))
+#    return redirect(url_for('index'))
 
