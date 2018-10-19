002b9baf0acee7ee8ee0066d6eb9a28ccfb46c61
pvonglehn <patrickvonglehn@gmail.com>
2018-10-19 15:30:52 +0000

    now getting flashcard data from text file

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,13 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 @app.route("/")
 def index():
-        my_pairs = (("Quiero que te (ir, tú) ya","Quiero que te vayas ya"),("Necesito que me (ayuar,tú) con esto","Quiero que me ayudes con esto"))
+        f = open("/home/patrickveegee/mysite/data/subjuntivo_finalidad.csv","r")
+        lines = f.readlines()
+        my_pairs = []
+        for line in lines:
+            words = line.split("\t")
+            my_pairs.append((words[0],words[1]))
+#        my_pairs = (("Quiero que te (ir, tú) ya","Quiero que te vayas ya"),("Necesito que me (ayuar,tú) con esto","Quiero que me ayudes con esto"))
         my_pair = my_pairs[random.randrange(len(my_pairs))]
         front = my_pair[0]
         back = my_pair[1]
